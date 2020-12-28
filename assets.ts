00fcffa02886144de1ca579ed31efe1a4b0e57ec
Adri314 <34138145+Adri314@users.noreply.github.com>
2020-12-28 09:19:24 -0500

    Initial files for MakeCode project

---
+++ assets.ts
@@ -0,0 +1,11 @@
+namespace skiing_spritesSprites {
+    //% fixedInstance jres blockIdentity=images._image
+    export const crash = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const front = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const right = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const left = image.ofBuffer(hex``);
+
+}
